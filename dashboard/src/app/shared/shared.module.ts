cebbd6758c25c078c0d1e9e4d20254a8e2f14a42
Himanshu055 <56827712+Himanshu055@users.noreply.github.com>
2022-02-05 12:05:40 +0530

    First Commit

---
+++ dashboard/src/app/shared/shared.module.ts
@@ -0,0 +1,50 @@
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {FlexLayoutModule} from '@angular/flex-Layout';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import { Router, RouterModule } from '@angular/router';
+import { AreaComponent } from './widgets/area/area.component';
+import {HighchartsChartModule} from 'highcharts-angular';
+import { CardComponent } from './widgets/card/card.component';
+import { PieComponent } from './widgets/pie/pie.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    AreaComponent,
+    CardComponent,
+    PieComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule,
+    HighchartsChartModule,
+  
+  ],
+  exports:[
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    AreaComponent,
+    CardComponent,
+    PieComponent
+  ]
+})
+export class SharedModule { }
